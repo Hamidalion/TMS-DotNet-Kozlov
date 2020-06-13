af7183b23c0da8dbac4a931cd6184af2af565b6e
Hamidalion <66638336+Hamidalion@users.noreply.github.com>
2020-06-13 18:23:33 +0300

    feat: <2>

--- src/Selfpreporation1/SelfPreporation1/Program.cs
+++ src/Selfpreporation1/SelfPreporation1/Program.cs
@@ -4,9 +4,33 @@ namespace SelfPreporation1
 {
     class Program
     {
+        enum DayOfWeek
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int monday = 1;
+            //int tuesday = 2;
+            //int Saturday = 2
+
+
+            Console.WriteLine("Now is");
+            Console.Write(DateTime.UtcNow); Console.WriteLine("(UTC)"); 
+            Console.WriteLine(DateTime.Now);
+            DateTime week = DateTime.Now;
+            Console.WriteLine(week.DayOfWeek);
+            Console.WriteLine("Select your favorite day of the week");
+            Console.WriteLine("Monday-1,Tuesday-2,Wednesday-3,Thursday-4,Friday-5,Saturday-6,Sunday-7");
+            Console.ReadLine();
+            
             Console.ReadKey();
         }
     }
