06e16c427d46570de5e95604f1d1113db15e7107
Hamidalion <66638336+Hamidalion@users.noreply.github.com>
2020-06-15 19:05:41 +0300

    Feat: <2>

--- src/Selfpreporation1/SelfPreporation1/Program.cs
+++ src/Selfpreporation1/SelfPreporation1/Program.cs
@@ -17,20 +17,44 @@ namespace SelfPreporation1
 
         static void Main(string[] args)
         {
-            //int monday = 1;
-            //int tuesday = 2;
-            //int Saturday = 2
-
+            //int Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 7;
+            int Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 7;
 
             Console.WriteLine("Now is");
             Console.Write(DateTime.UtcNow); Console.WriteLine("(UTC)"); 
             Console.WriteLine(DateTime.Now);
             DateTime week = DateTime.Now;
             Console.WriteLine(week.DayOfWeek);
+
             Console.WriteLine("Select your favorite day of the week");
             Console.WriteLine("Monday-1,Tuesday-2,Wednesday-3,Thursday-4,Friday-5,Saturday-6,Sunday-7");
-            Console.ReadLine();
             
+            int a = Convert.ToInt32(Console.ReadLine());
+          
+            if (a == 1)
+            {
+                Console.WriteLine("your favorite day of the week is Mondey and you are crazy");
+
+            }
+            else
+            {
+                Console.WriteLine($"Nomber {a} is odd");
+            }
+
+
+
+
+            //Console.WriteLine("You choose", 1 = Monday, 
+            
+                
+                
+                
+            //Console.WriteLine($"Hello {enum}");
+            //DayOfWeek op;
+            //op = DayOfWeek.Add;
+
+
+
             Console.ReadKey();
         }
     }
