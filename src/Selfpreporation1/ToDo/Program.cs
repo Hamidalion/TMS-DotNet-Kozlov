4bcc12618bae14f877a6c2cc6604f277208c4836
Hamidalion <66638336+Hamidalion@users.noreply.github.com>
2020-06-25 00:41:25 +0300

    fix <ToDo>
    Add new task of ToDo

---
+++ src/Selfpreporation1/ToDo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ToDo
+{
+    partial class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Inter your first task");
+            var todo1 = new ToDo(DateTime.Now)
+            {
+                Name = Console.ReadLine()
+            };
+           
+            Console.WriteLine(DateTime.Now.AddHours(3));
+
+            Console.WriteLine("Inter your second task");
+            var todo2 = new ToDo(DateTime.Now)
+            {
+                Name = Console.ReadLine()
+            };
+
+            todo1.Show();
+
+            todo2.Show();
+
+            Console.ReadKey();
+      
+        }
+    }
+}
