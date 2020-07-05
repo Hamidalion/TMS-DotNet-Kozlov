f76743924a8029ce7a2576fdd532bb25e301d4e1
Hamidalion <66638336+Hamidalion@users.noreply.github.com>
2020-07-06 00:37:39 +0300

    feat: Add new project FitnesTrack

---
+++ src/Selfpreporation1/Fitness.Core/Interfaces/IExerciseManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fitness.Core.Manager;
+using System.Linq;
+
+namespace Fitness.Core.Interfaces
+{
+    public interface IExerciseManager
+    {
+        double Jump(int count, double weight, int age);
+        double Run(double distance, double height, double weight, int age);
+        double Sweem(double distance, double height, double weight, int age);
+    }
+}
---
+++ src/Selfpreporation1/Fitness.Core/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fitness.Core.Models
+{
+    public class Person
+    {
+        public string Id { get; } = Guid.NewGuid().ToString();
+
+        public string Name { get; set; }
+
+        public double Weights { get; set; }
+
+        public double Height { get; set; }
+
+        public int Age { get; set; }
+
+        public IList<Result> ColoriesPerDay { get; set; } = new List<Result>();
+    }
+}
