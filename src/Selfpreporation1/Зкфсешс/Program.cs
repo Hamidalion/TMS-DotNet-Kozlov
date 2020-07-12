687eadeb8335794c37cb7e6ee0203b4956e71e03
Hamidalion <66638336+Hamidalion@users.noreply.github.com>
2020-07-12 21:16:32 +0300

    feat: Search my files in FitnesTrack

---
+++ src/Selfpreporation1/Зкфсешс/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Зкфсешс
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
